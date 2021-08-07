045cc78557f9f45a18811d6d3ec11f10b719158a
Glen Eustace <glen@eustace.nz>
2021-08-07 17:56:36 +1200

    Rewritten for EnaSolar Inverter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pyenasolar
-EnaSolar Connector for Homer Assistant
+
+This library was created to communicate with EnaSolar solar inverters within Home Assistant.
+It is based on the pysma and pysaj components written by @kellerza and @fredericvl
---
+++ pyenasolar/__init__.py
@@ -0,0 +1,195 @@
+"""PyEnaSolar interacts as a library to communicate with EnaSolar inverters"""
+import aiohttp
+import asyncio
+import concurrent
+from io import StringIO
+from datetime import date
+import logging
+import xml.etree.ElementTree as ET
+
+_LOGGER = logging.getLogger(__name__)
+
+MAPPER_STATES = {
+    "0": "Not connected",
+    "1": "Waiting",
+    "2": "Normal",
+    "3": "Error",
+    "4": "Upgrading",
+}
+
+URL_PATH_METERS = "meters.xml"
+URL_PATH_DATA   = "data.xml"
+
+class Sensor(object):
+    """Sensor definition"""
+
+    def __init__(self, key, factor, name, unit='',
+                 per_day_basis=False, per_total_basis=False):
+        self.key = key
+        self.factor = factor
+        self.name = name
+        self.unit = unit
+        self.value = None
+        self.per_day_basis = per_day_basis
+        self.per_total_basis = per_total_basis
+        self.date = date.today()
+        self.enabled = False
+
+
+class Sensors(object):
+    """EnaSolar sensors"""
+
+    def __init__(self):
+        self.__s = []
+        self.add(
+            (
+                Sensor("OutputPower", "", "output_power", "kWh"),
+                Sensor("InputVoltage", "", "input_voltage_1", "V"),
+                Sensor("Inputvoltage2", "", "input_voltage_2", "V"),
+                Sensor("OutputVoltage", "", "output_voltage", "V"),
+                Sensor("Irradiance", "", "irradiance", "W/m2"),
+                Sensor("Temperature", "", "tempertature", "C"),
+                Sensor("EnergyToday", "", "today_energy", "kWh", True),
+                Sensor("EnergyYesterday", "", "yesterday_energy", "kWh", True), 
+                Sensor("EnergyLifetime", "", "total_energy", "kWh", False, True),
+                Sensor("HoursExportedToday", "", "today_hours", "h", True),
+                Sensor("HoursExportedYesterday", "", "yesterday_hours", "h", True),
+                Sensor("HoursExportedLifetime", "", "total_hours", "h", False, True),
+            )
+        )
+
+    def __len__(self):
+        """Length."""
+        return len(self.__s)
+
+    def __contains__(self, key):
+        """Get a sensor using either the name or key."""
+        try:
+            if self[key]:
+                return True
+        except KeyError:
+            return False
+
+    def __getitem__(self, key):
+        """Get a sensor using either the name or key."""
+        for sen in self.__s:
+            if sen.name == key or sen.key == key:
+                return sen
+        raise KeyError(key)
+
+    def __iter__(self):
+        """Iterator."""
+        return self.__s.__iter__()
+
+    def add(self, sensor):
+        """Add a sensor, warning if it exists."""
+        if isinstance(sensor, (list, tuple)):
+            for sss in sensor:
+                self.add(sss)
+            return
+
+        if not isinstance(sensor, Sensor):
+            raise TypeError("pysenasolar.Sensor expected")
+
+        if sensor.name in self:
+            old = self[sensor.name]
+            self.__s.remove(old)
+            _LOGGER.warning("Replacing sensor %s with %s", old, sensor)
+
+        if sensor.key in self:
+            _LOGGER.warning("Duplicate EnaSolar sensor key %s", sensor.key)
+
+        self.__s.append(sensor)
+
+
+class EnaSolar(object):
+    """Provides access to EnaSolar inverter data"""
+
+    def __init__(self, host):
+        self.host = host
+
+        self.url = "http://{0}/".format(self.host)
+
+    async def read(self, sensors):
+        """Returns necessary sensors from EnaSolar inverter"""
+
+        try:
+            timeout = aiohttp.ClientTimeout(total=5)
+            async with aiohttp.ClientSession(timeout=timeout,
+                                             raise_for_status=True) as session:
+                current_url = self.url + URL_PATH_METERS
+
+                async with session.get(current_url) as response:
+                    data = await response.text()
+                    at_least_one_enabled = False
+
+                    xml = ET.fromstring(data)
+
+                    for sen in sensors:
+                       find = xml.find(sen.key)
+                           if find is not None:
+                               sen.value = find.text
+                               sen.date = date.today()
+                               sen.enabled = True
+                               at_least_one_enabled = True
+
+                    if not at_least_one_enabled:
+                        raise ET.ParseError
+
+                    if sen.enabled:
+                        _LOGGER.debug("Got new value for sensor %s: %s",
+                                      sen.name, sen.value)
+
+                current_url = self.url + URL_PATH_DATA
+
+                async with session.get(current_url) as response:
+                    data = await response.text()
+                    at_least_one_enabled = False
+
+                    xml = ET.fromstring(data)
+
+                    for sen in sensors:
+                       find = xml.find(sen.key)
+                           if find is not None:
+                               sen.value = find.text
+                               sen.date = date.today()
+                               sen.enabled = True
+                               at_least_one_enabled = True
+
+                    if not at_least_one_enabled:
+                        raise ET.ParseError
+
+                    if sen.enabled:
+                        _LOGGER.debug("Got new value for sensor %s: %s",
+                                      sen.name, sen.value)
+
+                    return True
+
+        except (aiohttp.client_exceptions.ClientConnectorError,
+                concurrent.futures._base.TimeoutError):
+            # Connection to inverter not possible.
+            # This can be "normal" - so warning instead of error - as SAJ
+            # inverters are powered by DC and thus have no power after the sun
+            # has set.
+            _LOGGER.warning("Connection to EnaSolar inverter is not possible. " +
+                            "Otherwise check host/ip address.")
+            return False
+
+        except aiohttp.client_exceptions.ClientResponseError as err:
+            # 401 Unauthorized: wrong username/password
+            if err.status == 401:
+                raise UnauthorizedException(err)
+            else:
+                raise UnexpectedResponseException(err)
+
+        except ET.ParseError:
+            # XML is not valid or even no XML at all
+            raise UnexpectedResponseException(
+                str.format("No valid XML received from {0} at {1}", self.host,
+                           current_url)
+            )
+
+class UnexpectedResponseException(Exception):
+    """Exception for unexpected status code"""
+    def __init__(self, message):
+        Exception.__init__(self, message)
